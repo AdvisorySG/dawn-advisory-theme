d3f05b097bbe22866dfb08570e3f60b9dc90b0ad
ongyongen <ongyongen2021@gmail.com>
2022-05-24 16:05:30 +0800

    edit home page text spelling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
-const homepageBackground = new URL("./img/homepage-bg.jpg", import.meta.url);
+import homepageBackground from "./img/homepage-bg.jpg";
 
 function Home () {
     return (
         <div className="home-page-container">
             <div>
                 <img src={homepageBackground} className="home-page-background"/>
-                <h1>studyPals</h1>
+                <h1>studyPal</h1>
                 <p>A productivity application to help you plan out your busy days!</p>
             </div>
         </div>
