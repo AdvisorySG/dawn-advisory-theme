1f05f4cde62d292ed70e57bb92b996e77056440e
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2022-05-26 00:20:56 +0800

    organise codes and files
    renamed style.css to index.css for index.js; moved App.js from components to src; created App.css under src for main css codes; changed code alignment in App.css

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Header from './components/Header.js';
+import Home from './components/Home.js';
+import Footer from './components/Footer.js';
+import Card from './components/Card.js';
+import UserTasks from './components/UserTasks.js';
+import Todolist from './components/Todolist.js';
+import './App.css';
+
+function Page () {
+    const userTasks = UserTasks.map(item => {
+        return <Card
+                    task = {item.task}
+                    day = {item.day}
+                    urgency = {item.urgency}
+                />
+    });
+
+    return (
+        <div class="container">
+            <Header />
+            <Todolist />
+            {userTasks}
+            <Footer />
+        </div>
+    );
+};
+
+export default Page;
