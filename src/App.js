7a14960db38cd03c4b73d4267a6f4a6a8e6e642d
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2022-05-26 23:54:08 +0800

    Update App.js, Todolist.js, index.js
    Changed home page after opening web; edited codes in Todolist.js; added some router config in index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import Header from './components/Header.js';
 import Home from './components/Home.js';
 import Footer from './components/Footer.js';
-import Card from './components/Card.js';
-import UserTasks from './components/UserTasks.js';
 import Todolist from './components/Todolist.js';
 import './App.css';
 
 function App() {
-    const userTasks = UserTasks.map(item => {
-        return <Card
-                    task = {item.task}
-                    day = {item.day}
-                    urgency = {item.urgency}
-                />
-    });
 
     return (
         <div class="container">
             <Header />
-            <Todolist />
-            {userTasks}
+            <Home />
             <Footer />
         </div>
     );
