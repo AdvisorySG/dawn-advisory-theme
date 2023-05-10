db08d76f9ce77b326efb24555a86ec3d750a31a0
Terence Chan Zun Mun <zunmun@gmail.com>
2023-05-10 09:18:22 +0800

    remove PhotoSwipe (#198)
    * remove HBS, CSS and JS references to PhotoSwipe
    * use ghost's default assets for .kg components

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 import './jquery-global.js';
 
 import InfiniteScroll from 'infinite-scroll';
-import PhotoSwipe from 'photoswipe';
-import PhotoSwipeUIDefault from 'photoswipe/dist/photoswipe-ui-default';
 import fitvids from 'fitvids';
 import 'lazysizes';
 
@@ -199,25 +197,6 @@ function video() {
     fitvids('.single-content');
 }
 
-function gallery() {
-    var images = document.querySelectorAll('.kg-gallery-image img');
-    images.forEach(function (image) {
-        var container = image.closest('.kg-gallery-image');
-        var width = image.attributes.width.value;
-        var height = image.attributes.height.value;
-        var ratio = width / height;
-        container.style.flex = ratio + ' 1 0%';
-    });
-
-    pswp(
-        '.kg-gallery-container',
-        '.kg-gallery-image',
-        '.kg-gallery-image',
-        false,
-        true
-    );
-}
-
 function table() {
     if (body.hasClass('post-template') || body.hasClass('page-template')) {
         var tables = $('.single-content').find('.table');
@@ -277,6 +256,17 @@ function modal() {
 
     modal.on('transitionend', function (e) {
         e.stopPropagation();
+    });
+}
+
+function gallery() {
+    const images = document.querySelectorAll('.kg-gallery-image img');
+    images.forEach((image) => {
+        const container = image.closest('.kg-gallery-image');
+        const width = image.attributes.width.value;
+        const height = image.attributes.height.value;
+        const ratio = width / height;
+        container.style.flex = ratio + ' 1 0%';
     });
 }
 
@@ -440,94 +430,5 @@ function search() {
 function burger() {
     $('.burger').on('click', function () {
         body.toggleClass('menu-opened');
-    });
-}
-
-function pswp(container, element, trigger, caption, isGallery) {
-    var parseThumbnailElements = function (el) {
-        var items = [],
-            gridEl,
-            linkEl,
-            item;
-
-        $(el)
-            .find(element)
-            .each(function (i, v) {
-                gridEl = $(v);
-                linkEl = gridEl.find(trigger);
-
-                item = {
-                    src: isGallery
-                        ? gridEl.find('img').attr('src')
-                        : linkEl.attr('href'),
-                    w: 0,
-                    h: 0,
-                };
-
-                if (caption && gridEl.find(caption).length) {
-                    item.title = gridEl.find(caption).html();
-                }
-
-                items.push(item);
-            });
-
-        return items;
-    };
-
-    var openPhotoSwipe = function (index, galleryElement) {
-        var pswpElement = document.querySelectorAll('.pswp')[0],
-            gallery,
-            options,
-            items;
-
-        items = parseThumbnailElements(galleryElement);
-
-        options = {
-            closeOnScroll: false,
-            history: false,
-            index: index,
-            shareEl: false,
-            showAnimationDuration: 0,
-            showHideOpacity: true,
-        };
-
-        gallery = new PhotoSwipe(
-            pswpElement,
-            PhotoSwipeUIDefault,
-            items,
-            options
-        );
-        gallery.listen('gettingData', function (index, item) {
-            if (item.w < 1 || item.h < 1) {
-                // unknown size
-                var img = new Image();
-                img.onload = function () {
-                    // will get size after load
-                    item.w = this.width; // set image width
-                    item.h = this.height; // set image height
-                    gallery.updateSize(true); // reinit Items
-                };
-                img.src = item.src; // let's download image
-            }
-        });
-        gallery.init();
-    };
-
-    var onThumbnailsClick = function (e) {
-        e.preventDefault();
-
-        var index = $(e.target)
-            .closest(container)
-            .find(element)
-            .index($(e.target).closest(element));
-        var clickedGallery = $(e.target).closest(container);
-
-        openPhotoSwipe(index, clickedGallery[0]);
-
-        return false;
-    };
-
-    $(container).on('click', trigger, function (e) {
-        onThumbnailsClick(e);
     });
 }
